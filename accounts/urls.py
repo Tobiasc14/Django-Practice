fb0b9567af7314ecb7813cd6987d323da9470f36
Tobias <spamalot7200@gmail.com>
2021-06-08 16:47:04 -0400

    *Hopefully* set up static images stuff

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('products/', views.products, name='products'),
     path('customer/', views.customer, name='customer'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
