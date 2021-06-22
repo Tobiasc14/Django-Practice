be87a48cd5cf69fa86a33a17b441eb68d5e1f885
Tobias <spamalot7200@gmail.com>
2021-06-22 18:00:18 -0400

    Changed navbar again, added two models (customer and products), tweaked formatting

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Products, Customer
+
+admin.site.register(Products)
+admin.site.register(Customer)
 
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Products(models.Model):
+    productName = models.CharField(max_length=200)
+    productImage = models.ImageField()
+
+class Customer(models.Model):
+    customerFirstName = models.CharField(max_length=200)
+    customerLastName = models.CharField(max_length=200)
+    purchasedProducts = models.ManyToManyField(Products)
+    dateJoined = models.DateTimeField('Date User Joined')
+
+
+
